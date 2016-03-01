8d70f48a9e64a8b42ea576786458af5c4578e8ad
Mike Bostock <mbostock@gmail.com>
2016-03-01 14:19:27 -0800

    Simpler version export.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export {version} from "./build/version";
 export {default as dsvFormat} from "./src/dsv";
 export {csvParse, csvParseRows, csvFormat, csvFormatRows} from "./src/csv";
 export {tsvParse, tsvParseRows, tsvFormat, tsvFormatRows} from "./src/tsv";

1fe389d49baf194a83a310c30a14565a3a257ec0
Mike Bostock <mbostock@gmail.com>
2016-06-07 16:52:28 -0700

    Export to d3.
    Related d3/d3#2840.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-export {version} from "./build/package";
 export {default as dsvFormat} from "./src/dsv";
 export {csvParse, csvParseRows, csvFormat, csvFormatRows} from "./src/csv";
 export {tsvParse, tsvParseRows, tsvFormat, tsvFormatRows} from "./src/tsv";

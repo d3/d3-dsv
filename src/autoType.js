c6b02ea84ae939c9db8d33d9712b6d56ba207fec
Mike Bostock <mbostock@gmail.com>
2019-02-07 11:11:33 -0800

    Shorten.

--- src/autoType.js
+++ src/autoType.js
@@ -1,21 +1,15 @@
 export default function autoType(object) {
   for (var key in object) {
-    var string = object[key].trim(), number;
-    if (/^(|null|NULL)$/.test(string)) {
-      object[key] = null;
-    } else if (/^(undefined|UNDEFINED)$/.test(string)) {
-      object[key] = undefined;
-    } else if (/^(true|TRUE)$/.test(string)) {
-      object[key] = true;
-    } else if (/^(false|FALSE)$/.test(string)) {
-      object[key] = false;
-    } else if (/^(NaN|NA[N]?|na[n]?)$/.test(string)) {
-      object[key] = NaN;
-    } else if (!isNaN(number = +string)) {
-      object[key] = number;
-    } else if (/^\d{4}(-\d{2}(-\d{2})?)?(T\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:\d{2})?)?$/.test(string)) {
-      object[key] = new Date(string);
-    }
+    var value = object[key].trim(), number;
+    if (/^(|null|NULL)$/.test(value)) value = null;
+    else if (/^(undefined|UNDEFINED)$/.test(value)) value = undefined;
+    else if (/^(true|TRUE)$/.test(value)) value = true;
+    else if (/^(false|FALSE)$/.test(value)) value = false;
+    else if (/^(NaN|NA[N]?|na[n]?)$/.test(value)) value = NaN;
+    else if (!isNaN(number = +value)) value = number;
+    else if (/^\d{4}(-\d{2}(-\d{2})?)?(T\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:\d{2})?)?$/.test(value)) value = new Date(value);
+    else continue;
+    object[key] = value;
   }
   return object;
 }

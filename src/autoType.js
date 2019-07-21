970f0e788b5edcdd59d84bbf217d6cd2a1926522
Philippe Rivière <fil@rezo.net>
2019-07-21 19:04:09 +0200

    Compute tzFix once on startup rather than every time autoType is invoked.
    Store the local timezone offset string (e.g., -07:00) rather than a boolean, or null if the workaround is not needed.
    
    Add the local timezone offset string to the input value, when m[4] is truthy (there is a time) and m[7] is falsy (there is no TZ information).

--- src/autoType.js
+++ src/autoType.js
@@ -1,21 +1,32 @@
 export default function autoType(object) {
-  var m,
-    tzFix = (new Date("2019-01-01T00:00").getHours() || new Date("2019-07-01T00:00").getHours())
-      ? new Date().getTimezoneOffset() * 60000
-      : false;
   for (var key in object) {
-    var value = object[key].trim(), number;
+    var value = object[key].trim(), number, m;
     if (!value) value = null;
     else if (value === "true") value = true;
     else if (value === "false") value = false;
     else if (value === "NaN") value = NaN;
     else if (!isNaN(number = +value)) value = number;
     else if (m = value.match(/^([-+]\d{2})?\d{4}(-\d{2}(-\d{2})?)?(T\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:\d{2})?)?$/)) {
+      if (fixtz && !!m[4] && !m[7]) value += fixtz;
       value = new Date(value);
-      if (tzFix && !m[7]) value = new Date(+value + tzFix);
     }
     else continue;
     object[key] = value;
   }
   return object;
+}
+
+// https://github.com/d3/d3-dsv/issues/45
+var fixtz = tzoffset();
+
+function tzoffset() {
+  if (new Date("2019-01-01T00:00").getHours() || new Date("2019-07-01T00:00").getHours()) {
+    var offset = -new Date().getTimezoneOffset() / 60;
+    return (
+        offset < -9 ? "-" + -offset
+      : offset < 0 ? "-0" + -offset
+      : offset < 10 ? "+0" + offset
+      : "+" + offset
+    ) + ":00";
+  }
 }

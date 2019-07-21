a32f6df7ba255f305f98461b64c6282570e0cc2b
Philippe Rivière <fil@rezo.net>
2019-07-21 16:03:50 +0200

    Incorrect decoding of local dates in Safari.
    instead of treating `new Date("2018-04-25T11:00")` as a local date, Safari parses it as a UTC date and returns it with the tz difference added. This fix detects the situation by checking TWO local dates (one in winter, one in summer) which should return hour 0, and if one of them is not 0 it's the Safari bug. In that case, and if the date string that is passed is local (no "Z"), we shift it accordingly.
    
    I don't know how to write a node test for this, but the following notebook shows the fix on Safari:
    https://observablehq.com/d/7e8b78e5050258ae
    
    Fixes https://github.com/d3/d3-dsv/issues/45

--- src/autoType.js
+++ src/autoType.js
@@ -1,4 +1,8 @@
 export default function autoType(object) {
+  var m,
+    tzFix = (new Date("2019-01-01T00:00").getHours() || new Date("2019-07-01T00:00").getHours())
+      ? new Date().getTimezoneOffset() * 60000
+      : false;
   for (var key in object) {
     var value = object[key].trim(), number;
     if (!value) value = null;
@@ -6,7 +10,10 @@ export default function autoType(object) {
     else if (value === "false") value = false;
     else if (value === "NaN") value = NaN;
     else if (!isNaN(number = +value)) value = number;
-    else if (/^([-+]\d{2})?\d{4}(-\d{2}(-\d{2})?)?(T\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:\d{2})?)?$/.test(value)) value = new Date(value);
+    else if (m = value.match(/^([-+]\d{2})?\d{4}(-\d{2}(-\d{2})?)?(T\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:\d{2})?)?$/)) {
+      value = new Date(value);
+      if (tzFix && !m[7]) value = new Date(+value + tzFix);
+    }
     else continue;
     object[key] = value;
   }
